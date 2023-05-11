f225f445d4941820ae4272113580957d1f9f00b0
Valentin <valentinperezcuevas@gmail.com>
2023-05-11 10:10:13 +0200

    Añadido botones al final que permiten filtrar los resultados

--- models/UI.js
+++ models/UI.js
@@ -66,11 +66,13 @@ export class UI {
       document.getElementById("footer").remove();
       
     })
+
+    
 /* *******************************
         Creación de tabla
 ********************************** */
+let H3Tabla1 = document.createTextNode("Tabla de puntuaciones")
 let H3Tabla = document.createElement("legend")
-let H3Tabla1 = document.createTextNode("Puntuacion Jugadores")
 H3Tabla.appendChild(H3Tabla1)
 
 let nombres = [];
@@ -106,10 +108,10 @@ for(let i = 0; i < resultado.length; i++){
 console.log(resultado);
     
     console.log(H1Resultado,puntuacion)
+    
     element1.appendChild(H3Tabla)
     element1.appendChild(H1Resultado)
     element1.appendChild(puntuacion)
-
    /* *******************************
             Botón para volver al inicio
     ********************************** */
@@ -121,9 +123,159 @@ console.log(resultado);
               window.location.href = "index.html";
             });
             
-            element1.appendChild(btnInicio);
+            element1.appendChild(btnInicio);  
+
+   /* *******************************
+            Botón para mostrar ultimos 5
+    ********************************** */
+            const btnMostrar5Ultimos = document.createElement("button");
+            const btmMostrar5UltimosTexto = document.createTextNode("Últimos 5 resultados");
+            btnMostrar5Ultimos.appendChild(btmMostrar5UltimosTexto);
+
+            btnMostrar5Ultimos.addEventListener("click", () => {
+              // Obtener los 5 primeros resultados
+              //const ult5 = resultado.slice(0, 5);
+              const ult5 = resultado.slice(Math.max(resultado.length - 5, 0));
+
+              // Crear una nueva tabla con los 5 primeros resultados
+              const tablaTop5 = document.createElement("table");
+              const trHeader = document.createElement("tr");
+              const thNombre = document.createElement("th");
+              const thPuntuacion = document.createElement("th");
+              const thNombreText = document.createTextNode("Nombre");
+              const thPuntuacionText = document.createTextNode("Puntuación");
+              
+              thNombre.appendChild(thNombreText);
+              thPuntuacion.appendChild(thPuntuacionText);
+              trHeader.appendChild(thNombre);
+              trHeader.appendChild(thPuntuacion);
+              tablaTop5.appendChild(trHeader);
+              
+              for(let i = 0; i < ult5.length; i++){
+                const tr = document.createElement("tr");
+                const tdNombre = document.createElement("td");
+                const tdPuntuacion = document.createElement("td");
+                const tdNombreText = document.createTextNode(ult5[i].nombre);
+                const tdPuntuacionText = document.createTextNode(ult5[i].puntuacion);
+                
+                tdNombre.appendChild(tdNombreText);
+                tdPuntuacion.appendChild(tdPuntuacionText);
+                tr.appendChild(tdNombre);
+                tr.appendChild(tdPuntuacion);
+                tablaTop5.appendChild(tr);
+              }
+              
+              // Agregar la nueva tabla a la página
+              element1.appendChild(tablaTop5);
+            });
             
+
+            element1.appendChild(btnMostrar5Ultimos);
+
+
+  /* *******************************
+            Botón para mostrar top 5
+             puntuaciones
+    ********************************** */
+
+             const btnMostrarTop5Altas = document.createElement("button");
+             const btnMostrarTop5AltasTexto = document.createTextNode("Top 5 puntuaciones más altas");
+             btnMostrarTop5Altas.appendChild(btnMostrarTop5AltasTexto);
+             
+             btnMostrarTop5Altas.addEventListener("click", () => {
+               // Ordenar el array de resultados según la puntuación
+               const sorted = resultado.sort((a, b) => b.puntuacion - a.puntuacion);
+               
+               // Seleccionar los primeros 5 elementos
+               const top5 = sorted.slice(0, 5);
+               
+               // Crear una nueva tabla con los 5 primeros resultados
+               const tablaTop5 = document.createElement("table");
+               const trHeader = document.createElement("tr");
+               const thNombre = document.createElement("th");
+               const thPuntuacion = document.createElement("th");
+               const thNombreText = document.createTextNode("Nombre");
+               const thPuntuacionText = document.createTextNode("Puntuación");
+                           
+               thNombre.appendChild(thNombreText);
+               thPuntuacion.appendChild(thPuntuacionText);
+               trHeader.appendChild(thNombre);
+               trHeader.appendChild(thPuntuacion);
+               tablaTop5.appendChild(trHeader);
+                           
+               for(let i = 0; i < top5.length; i++){
+                 const tr = document.createElement("tr");
+                 const tdNombre = document.createElement("td");
+                 const tdPuntuacion = document.createElement("td");
+                 const tdNombreText = document.createTextNode(top5[i].nombre);
+                 const tdPuntuacionText = document.createTextNode(top5[i].puntuacion);
+                             
+                 tdNombre.appendChild(tdNombreText);
+                 tdPuntuacion.appendChild(tdPuntuacionText);
+                 tr.appendChild(tdNombre);
+                 tr.appendChild(tdPuntuacion);
+                 tablaTop5.appendChild(tr);
+               }
+                           
+               // Agregar la nueva tabla a la página
+               element1.appendChild(tablaTop5);
+             });
+             
+             element1.appendChild(btnMostrarTop5Altas);
+             
+        /* *******************************
+            Botón para mostrar 5
+            mas bajas
+    ********************************** */
+
+            const btnMostrar5MasBajas = document.createElement("button");
+const btnMostrar5MasBajasTexto = document.createTextNode("5 puntuaciones más bajas");
+btnMostrar5MasBajas.appendChild(btnMostrar5MasBajasTexto);
+
+btnMostrar5MasBajas.addEventListener("click", () => {
+  // Ordenar los resultados de menor a mayor
+  const resultadosOrdenados = resultado.sort((a, b) => a.puntuacion - b.puntuacion);
+
+  // Obtener los 5 primeros resultados
+  const ult5 = resultadosOrdenados.slice(0, 5);
+
+  // Crear una nueva tabla con los 5 primeros resultados
+  const tablaTop5 = document.createElement("table");
+  const trHeader = document.createElement("tr");
+  const thNombre = document.createElement("th");
+  const thPuntuacion = document.createElement("th");
+  const thNombreText = document.createTextNode("Nombre");
+  const thPuntuacionText = document.createTextNode("Puntuación");
+
+  thNombre.appendChild(thNombreText);
+  thPuntuacion.appendChild(thPuntuacionText);
+  trHeader.appendChild(thNombre);
+  trHeader.appendChild(thPuntuacion);
+  tablaTop5.appendChild(trHeader);
+
+  for(let i = 0; i < ult5.length; i++){
+    const tr = document.createElement("tr");
+    const tdNombre = document.createElement("td");
+    const tdPuntuacion = document.createElement("td");
+    const tdNombreText = document.createTextNode(ult5[i].nombre);
+    const tdPuntuacionText = document.createTextNode(ult5[i].puntuacion);
+
+    tdNombre.appendChild(tdNombreText);
+    tdPuntuacion.appendChild(tdPuntuacionText);
+    tr.appendChild(tdNombre);
+    tr.appendChild(tdPuntuacion);
+    tablaTop5.appendChild(tr);
   }
+
+  // Agregar la nueva tabla a la página
+  element1.appendChild(tablaTop5);
+});
+
+element1.appendChild(btnMostrar5MasBajas);
+
+        
+      }
+  
 
 
 
